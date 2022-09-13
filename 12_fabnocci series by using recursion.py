6a541d1d978c4b5a96d640a4230a9275b93c3654
Anuj saxena <engg.anujsaxena@gmail.com>
2022-09-13 23:14:29 +0530

    2nd commit

---
+++ 12_fabnocci series by using recursion.py
@@ -0,0 +1,13 @@
+
+def fibonacci(num):
+    """
+    Recursive function to print fibonacci sequence
+    """
+    return num if num <= 1 else fibonacci(num - 1) + fibonacci(num - 2)
+
+nterms = 10
+
+print "Fibonacci sequence"
+
+for num in range(numbers):
+    print fibonacci(num)
